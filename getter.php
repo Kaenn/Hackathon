c13ef58bc4fffe47281e182f3aab852dae764149
Glenn Inizan <glenn.inizan@fr.clara.net>
2015-05-25 14:55:57 +0200

    Mise en place du getter

---
+++ getter.php
@@ -0,0 +1,41 @@
+<?php
+
+	if(isset($_GET['action'])){
+		$action=$_GET['action'];
+	}else{
+		$action="undefined";
+	}
+
+	$date=new DateTime();
+	$ip=$_SERVER["REMOTE_ADDR"];
+	
+	
+	$bdd = new PDO('mysql:host=localhost;dbname=hackathon;charset=utf8', 'root', '');
+	
+	
+	$insert_action = "INSERT INTO getter_action 
+					(name,date,ip) 
+					VALUES 
+					(:name,:date,:ip)";
+	$stmt = $bdd->prepare($insert_action);
+	$stmt->bindParam(':name', $action, PDO::PARAM_STR);
+	$stmt->bindParam(':date', $date->format("Y-m-d H:i:s"));
+	$stmt->bindParam(':ip', $ip);
+	$stmt->execute();
+	$action_id=$bdd->lastInsertId();
+	
+	$inser_value="INSERT INTO getter_value 
+					(id_action,name,value) 
+					VALUES 
+					(:id_action,:name,:value)";
+	foreach($_GET as $name => $value){
+		if($name!="action"){
+			$stmt = $bdd->prepare($inser_value);
+			$stmt->bindParam(':id_action', $action_id, PDO::PARAM_INT);
+			$stmt->bindParam(':name', $name,PDO::PARAM_STR);
+			$stmt->bindParam(':value', $value,PDO::PARAM_STR);
+			$stmt->execute();
+		}
+	}
+	
+?>
